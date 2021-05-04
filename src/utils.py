6d5722ba2634ecb62ae8fda128d4a6e0028fce27
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-05-03 17:20:21 -0700

    Added utils with surface to array conversions.

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+#
+#  Piano Video
+#  Piano MIDI visualizer
+#  Copyright Patrick Huang 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import numpy as np
+import pygame
+pygame.init()
+
+
+def surf_to_array(surf: pygame.Surface) -> np.ndarray:
+    return pygame.surfarray.array3d(surf).swapaxes(0, 1)
+
+def array_to_surf(array: np.ndarray) -> pygame.Surface:
+    return pygame.image.frombuffer(array.tobytes(), array.shape[1::-1], "RGB")
