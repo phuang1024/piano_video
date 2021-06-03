bbc4582bb3183b4e5dd613dbe83c24d6bb916217
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-06-03 12:34:07 -0700

    Added game loop.

---
+++ src/main.py
@@ -0,0 +1,44 @@
+#
+#  Piano Video
+#  Piano MIDI visualizer
+#  Copyright Patrick Huang 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+
+import pygame
+from utils import *
+pygame.init()
+
+
+def main():
+    surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+    clock = pygame.time.Clock()
+    pygame.display.set_caption(f"Piano Video {VERSION}")
+    pygame.display.set_icon(IMAGES["icon"])
+
+    while True:
+        clock.tick(FPS)
+        pygame.display.update()
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+
+main()
