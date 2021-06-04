d9e0cf227f921530a8602244c9c496cb6518cd3b
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-06-04 13:05:39 -0700

    Added property and bool prop.

--- src/pv/props.py
+++ src/pv/props.py
@@ -16,3 +16,43 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+
+import io
+from typing import Literal
+
+
+class Property:
+    type_id: int
+
+    idname: str
+    label: str
+    description: str
+
+    def __init__(self, idname: str = "", label: str = "", description: str = "") -> None:
+        self.idname = idname
+        self.label = label
+        self.description = description
+
+    def dump(self, stream: io.BytesIO) -> None:...
+
+    def load(self, stream: io.BytesIO) -> None:...
+
+
+class BoolProp(Property):
+    type_id: Literal[0] = 0
+
+    default: bool
+    value: bool
+
+    def __init__(self, idname: str = "", label: str = "", description: str = "",
+            default: bool = False, value: bool = False) -> None:
+        super().__init__(idname, label, description)
+        self.default = default
+        self.value = default if value is None else value
+
+    def dump(self, stream: io.BytesIO) -> None:
+        stream.write(bytes([self.type_id, self.value]))
+
+    def load(self, stream: io.BytesIO) -> None:
+        assert stream.read(1)[0] == self.type_id, f"Type ID for BoolProp must be {self.type_id}"
+        self.value = (stream.read(1) == b"\x01")
