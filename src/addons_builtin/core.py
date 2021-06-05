bad06f3a691d092eb2616505096bb2eb618b9734
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-06-04 18:09:10 -0700

    Added output properties. Output resolution and fps.

---
+++ src/addons_builtin/core.py
@@ -0,0 +1,66 @@
+#
+#  Piano Video
+#  Piano MIDI visualizer
+#  Copyright Patrick Huang 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""
+Contains core properties, operators, and UI,
+such as output properties.
+"""
+
+import pv
+from pv.props import *
+
+
+class OUTPUT_PT_Props(pv.types.PropertyGroup):
+    idname = "output"
+
+    props = [
+        IntProp(
+            idname="res_x",
+            label="X Resolution",
+            description="Resolution (pixels) of output X",
+            default=1920, min=1, max=65536,
+        ),
+
+        IntProp(
+            idname="res_y",
+            label="Y Resolution",
+            description="Resolution (pixels) of output Y",
+            default=1080, min=1, max=65536,
+        ),
+
+        IntProp(
+            idname="fps",
+            label="FPS",
+            description="Output frames per second",
+            default=30, min=1, max=1000,
+        )
+    ]
+
+
+classes = (
+    OUTPUT_PT_Props,
+)
+
+def register():
+    for cls in classes:
+        pv.utils.register_class(cls)
+
+def unregister():
+    for cls in classes:
+        pv.utils.unregister_class(cls)
