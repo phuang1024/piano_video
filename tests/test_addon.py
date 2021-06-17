d543154691aad3944070b07453b5b19ed1a1243a
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-06-16 19:51:42 -0700

    Added test add-on.

---
+++ tests/test_addon.py
@@ -0,0 +1,63 @@
+pv_info = {
+    "name": "test",
+}
+
+import pv
+from pv.props import *
+
+
+class TEST_PT_Props(pv.types.PropertyGroup):
+    idname = "test"
+
+    props = [
+        BoolProp(
+            idname="prop",
+            label="A boolean property",
+            description="hi"
+        )
+    ]
+
+
+class TEST_UT_Section(pv.types.UISection):
+    idname = "test"
+    label = "A test"
+    description = "..."
+
+
+class TEST_UT_Panel(pv.types.UIPanel):
+    idname = "test"
+    label = "My Panel"
+    description = "blahblahblah"
+    section_id = "test"
+
+    def draw(self):
+        layout = self.layout
+        layout.prop("test.prop")
+        layout.label("This is a label")
+        layout.operator("test.operator")
+
+
+class TEST_OT_Operator(pv.types.Operator):
+    idname = "test.operator"
+    label = "An operator"
+    description = "hi"
+
+    def execute(self):
+        self.report("INFO", "Ran the operator")
+        return "FINISHED"
+
+
+classes = (
+    TEST_PT_Props,
+    TEST_UT_Section,
+    TEST_UT_Panel,
+    TEST_OT_Operator,
+)
+
+def register():
+    for cls in classes:
+        pv.utils.register_class(cls)
+
+def unregister():
+    for cls in classes:
+        pv.utils.unregister_class(cls)
