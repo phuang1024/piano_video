d94171530d5565e138cd77f9a7959d3dd4c6cd2d
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-07-01 13:44:44 -0700

    Added data namespace.

--- src/pva/server.py
+++ src/pva/server.py
@@ -19,8 +19,44 @@
 
 import os
 import argparse
+import base64
+import json
 from hashlib import sha256, sha384, sha512
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
+PARENT = os.path.dirname(os.path.realpath(__file__))
+
+
+class Data:
+    @staticmethod
+    def read(path: str, mode: str = "r"):
+        with open(os.path.join(PARENT, path), mode) as file:
+            return file.read()
+
+    @staticmethod
+    def write(path: str, data, mode: str = "w"):
+        with open(os.path.join(PARENT, path), mode) as file:
+            file.write(data)
+
+    @staticmethod
+    def isfile(path: str):
+        return os.path.isfile(os.path.join(PARENT, path))
+
+    @staticmethod
+    def isdir(path: str):
+        return os.path.isdir(os.path.join(PARENT, path))
+
+    @staticmethod
+    def makedirs(path: str):
+        os.makedirs(os.path.join(PARENT, path))
+
+    @staticmethod
+    def load(path: str):
+        return json.loads(Data.read(path))
+
+    @staticmethod
+    def dump(path: str, obj):
+        Data.write(path, json.dumps(obj))
 
 
 class Handler(BaseHTTPRequestHandler):
@@ -46,7 +82,7 @@ def secure_hash(data: bytes) -> bytes:
 
 
 def main():
-    parser = argparse.ArgumentParser("Server of Piano Video add-on system.")
+    parser = argparse.ArgumentParser(description="Server of Piano Video add-on system.")
     parser.add_argument("--ip", help="IP address to bind to", default="0.0.0.0")
     parser.add_argument("--port", help="Port to bind to", type=int, default=5555)
     args = parser.parse_args()
