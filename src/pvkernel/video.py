ecae042d66f99819493066c8cc7d43fb4228c9e4
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-08-04 21:54:15 -0700

    Kernel: Video class. Contains resolution and fps for now.

---
+++ src/pvkernel/video.py
@@ -0,0 +1,40 @@
+#
+#  Piano Video
+#  A free piano visualizer.
+#  Copyright Patrick Huang 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from typing import Tuple
+
+
+class Video:
+    """
+    This class holds all the settings of a video.
+    It is also used like a "context" for all rendering functions.
+    """
+    resolution: Tuple[int, int]
+    fps: float
+
+    def __init__(self, resolution: Tuple[int, int] = (1920, 1080), fps: float = 30.) -> None:
+        """
+        Initializes the Video.
+
+        :param resolution: (X, Y) pixel resolution.
+        :param fps: Frames per second. Can be float, but will be rounded down in export.
+        :return: None
+        """
+        self.resolution = resolution
+        self.fps = fps
