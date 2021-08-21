8e9bc979b516a260ee2dc43a7b5ff88ee79f9717
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-08-21 10:18:20 -0700

    Keyboard: Video initialize and destruct. Also video read function.

--- src/pvkernel/addons/keyboard/__init__.py
+++ src/pvkernel/addons/keyboard/__init__.py
@@ -18,13 +18,11 @@
 #
 
 """
-Core properties and operators.
-
-Will register:
-
-* Property group ``keyboard``
+Keyboard rendering.
 """
 
+import numpy as np
+import cv2
 import pv
 from pv.props import FloatProp, StrProp
 from pvkernel import Video
@@ -72,15 +70,51 @@ class KEYBOARD_OT_Render(pv.Operator):
         pass
 
 
+class KEYBOARD_DT_Data(pv.DataGroup):
+    idname = "keyboard"
+
+
 class KEYBOARD_JT_Init(pv.Job):
     idname = "keyboard_init"
 
     def execute(self, video: Video) -> None:
-        pass
+        path = video.props.keyboard.video_path
+        video.data.keyboard.video = cv2.VideoCapture(path)
+        video.data.keyboard.fps = video.data.keyboard.video.fps
+
+
+class KEYBOARD_JT_Deinit(pv.Job):
+    idname = "keyboard_init"
+
+    def execute(self, video: Video) -> None:
+        video.data.keyboard.video.release()
+
+
+def read_frame(video: Video, frame: int) -> np.ndarray:
+    """
+    Read a frame from ``video.data.keyboard.video``.
+
+    :param video: The video class instance.
+    :param frame: Frame to read. 0 = when the first block starts playing.
+    """
+    data = video.data.keyboard
+    props = video.props.keyboard
+
+    real_frame = props.video_start*data.fps + frame
+    data.video.set(1, real_frame)
+
+    ret, img = data.video.read()
+    assert ret, "VideoCapture read failed."
+
+    return img
 
 
 classes = (
     KEYBOARD_PT_Props,
+    KEYBOARD_OT_Render,
+    KEYBOARD_DT_Data,
+    KEYBOARD_JT_Init,
+    KEYBOARD_JT_Deinit,
 )
 
 def register():
