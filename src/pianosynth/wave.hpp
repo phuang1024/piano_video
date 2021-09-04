bb53977f0a92f6250f633ce4681c9aeac5ee9ada
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-09-03 23:23:24 -0700

    Synth: Wave audio write class.

---
+++ src/pianosynth/wave.hpp
@@ -0,0 +1,53 @@
+//
+//  Piano Video
+//  A free piano visualizer.
+//  Copyright Patrick Huang 2021
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include <fstream>
+#include <string>
+
+
+namespace Wave {
+
+/**
+ * Write wave file. File format and specifications are static:
+ * 32 bit
+ * 1 channel
+ */
+class WaveWrite {
+public:
+    /**
+     * Calls close() if not closed.
+     */
+    ~WaveWrite();
+
+    /**
+     * Initialize with output file path.
+     */
+    WaveWrite(const std::string path);
+
+    /**
+     * Must be called after done writing.
+     */
+    void close();
+
+private:
+    bool _open;
+    std::ofstream _fp;
+};
+
+}  // namespace Wave
